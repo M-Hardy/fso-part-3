ad3759f399a79d6ec4516f2a178c7850b767561e
Michael <mbhardy001@gmail.com>
2024-03-20 14:51:20 -0400

    Update fetching of phonebook entries so that data is fetched from MongoDB database.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Contact = require("./models/contact");
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
@@ -8,58 +11,35 @@ app.use(express.json());
 app.use(express.static("dist"));
 app.use(morgan("tiny"));
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
-
-// "/" requests the root/homepage of a web app. For a web server, "/" refers
-// to the root directory or default landing page of the server
+// Overridden with index.html from frontend in dist folder
+// using express.static('dist')
 app.get("/", (request, response) => {
     response.send("<h1>This is the application's root directory</h1>");
 });
 
 app.get("/info", (request, response) => {
-    const numPersons = persons.length;
+    const numContacts = Contact.find({}).then((contacts) => {
+        return contacts.length;
+    });
     const requestTime = new Date().toString();
     response.send(
-        `<p>Phonebook has info for ${numPersons} people</p><p>${requestTime}</p>`
+        `<p>Phonebook has info for ${numContacts} people</p><p>${requestTime}</p>`
     );
 });
 
-app.get("/api/persons", (request, response) => {
-    response.json(persons);
+app.get("/api/contacts", (request, response) => {
+    Contact.find({}).then((contacts) => {
+        response.json(contacts);
+    });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((person) => person.id === id);
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+app.get("/api/contacts/:id", (request, response) => {
+    Contact.findById(request.params.id).then((contact) => {
+        response.json(contact);
+    });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/contacts/:id", (request, response) => {
     const id = Number(request.params.id);
     persons = persons.filter((person) => person.id !== id);
     response.status(204).end();
@@ -70,7 +50,7 @@ const generateId = (max = 10000) => {
     return id;
 };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/contacts", (request, response) => {
     const body = request.body;
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -94,7 +74,6 @@ app.post("/api/persons", (request, response) => {
     response.json(newPerson);
 });
 
-const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
 });
